aaa61e1cf89fc9c2c13b773c8199ca587ed37435
cu8code <git8ankanroy@gmail.com>
2024-09-19 21:23:34 +0530

    files relocated to client

--- antlr/build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-export antlr='java -jar /home/ankan/Documents/antlr-4.13.2-complete.jar'
-
-$antlr -Dlanguage=Go /home/ankan/Documents/git/me/9tail/antlr/NinetailLexer.g4 -o parser
-$antlr -Dlanguage=Go /home/ankan/Documents/git/me/9tail/antlr/NinetailParser.g4 -visitor -o parser
